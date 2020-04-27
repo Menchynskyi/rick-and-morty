cf04298262e98e6e1a5a0547e47f912337503de5
Oleksandr Menchynskyi <the.mngs@yahoo.com>
2020-04-27 16:41:03 +0300

    Change inputs on lacations page

--- src/pages/Locations/FilterLocations/FilterLocations.tsx
+++ src/pages/Locations/FilterLocations/FilterLocations.tsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import {
   FilterContainer,
-  InputStyled,
+  InputContainer,
   ButtonContainer,
 } from './FilterLocationsStyled';
 import { useLocationDispatch } from '../../../contexts';
-import { Button } from '../../../components';
+import { Button, Input } from '../../../components';
 
 const initialInputState = {
   name: '',
@@ -49,27 +49,37 @@ export const FilterLocations: React.FC = () => {
 
   return (
     <FilterContainer>
-      <InputStyled
-        name="name"
-        placeholder="Name"
-        autoComplete="off"
-        value={inputState.name}
-        onChange={handleChange}
-      />
-      <InputStyled
-        name="type"
-        placeholder="Type"
-        autoComplete="off"
-        value={inputState.type}
-        onChange={handleChange}
-      />
-      <InputStyled
-        name="dimension"
-        placeholder="Dimension"
-        autoComplete="off"
-        value={inputState.dimension}
-        onChange={handleChange}
-      />
+      <InputContainer>
+        <Input
+          name="name"
+          placeholder="Name"
+          autoComplete="off"
+          value={inputState.name}
+          onChange={handleChange}
+          fullWidth
+        />
+      </InputContainer>
+      <InputContainer>
+        <Input
+          name="type"
+          placeholder="Type"
+          autoComplete="off"
+          value={inputState.type}
+          onChange={handleChange}
+          fullWidth
+        />
+      </InputContainer>
+      <InputContainer>
+        <Input
+          name="dimension"
+          placeholder="Dimension"
+          autoComplete="off"
+          value={inputState.dimension}
+          onChange={handleChange}
+          fullWidth
+        />
+      </InputContainer>
+
       <ButtonContainer>
         <Button
           color="secondary"
